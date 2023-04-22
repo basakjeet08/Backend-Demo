1370ef3f3605ad55c4e970c7a1cab477ec7c60c5
Anirban Basak <jeetbasak2002@gmail.com>
2023-04-22 17:25:20 +0530

    initial commit

---
+++ src/test/kotlin/com/anirban/backenddemo/BackendDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.anirban.backenddemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendDemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
